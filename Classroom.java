f0bf407acb0767468f24101dd7d62d43fb092347
omar <omszag@hotmail.fr>
2019-09-29 13:43:56 +0200

    envoie des classes

--- Classroom.java
+++ Classroom.java
@@ -3,11 +3,11 @@ public class Classroom {
   public static void main(String [] args) {
       
 
-        Wilder Jean-claude = new Wilder("Jean-claude", false);
-          System.out.println("1. " + Jean-claude.whoAmI());
+        Wilder Jean_claude = new Wilder("Jean_claude", true);
+          System.out.println("1. " + Jean_claude.whoAmI());
 
-        julien.setAware(true);
-          System.out.println("2. " + Henry.whoAmI());
+	Wilder Henry = new Wilder("Henry", false);
+	  System.out.println("2. " + Henry.whoAmI());
   
   
   }
--- Wilder.java
+++ Wilder.java
@@ -1,41 +1,40 @@
-public class wilder {
-  
-  private String firstName;
-  private boolean aware;
-  
-  
-      public Wilder(String firstName) {
-        this.firstName = firstName;
+class Wilder {
+
+    
+    private String firstname;
+    private boolean aware;
+
+    public Wilder(String firstname) {
+        this.firstname = firstname;
         this.aware = true;
     }
 
-      public Wilder(String firstName, boolean aware) {
-        this.firstName = firstName;
+    public Wilder(String firstname, boolean aware) {
+        this.firstname = firstname;
         this.aware = aware;
     }
-  
-      public String getfirstName() {
-          return this.firstName;
+
+    public String getFirstName() {
+        return this.firstname;
     }
-      
-      public boolean isaware() {
-          return this.aware;
+
+    public boolean isAware() {
+        return this.aware;
     }
-    
-      public void String setName(String firstName) {
-          this.firstName = firstName;
+
+    public void setName(String firstname) {
+        this.firstname = firstname;
     }
-      
-      public void boolean setAware(boolean aware) {
-          this.aware = aware;  
+
+    public void setAware(boolean aware) {
+        this.aware = aware;
     }
-    
-    
-      public String whoAmI() {
-          if (this.aware) {
-              return "My name is " + this.getFirstName() + " and I'm aware";
-          } else {
-              return "My name is " + this.getFirstName() + " and I'm not aware";
-            }
+
+    public String whoAmI() {
+        if (this.aware) {
+            return "My name is " + this.getFirstName() + " and I'm aware";
+        } else {
+            return "My name is " + this.getFirstName() + " and I'm not aware";
+        }
     }
 }
