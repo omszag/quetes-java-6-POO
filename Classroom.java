85b7a59964729512b382ef9520c667b35668e0f5
omszag <53254676+omszag@users.noreply.github.com>
2019-09-29 12:05:25 +0200

    Create Classroom.java

---
+++ Classroom.java
@@ -0,0 +1,16 @@
+public class Classroom {
+
+  public static void main(String [] args) {
+      
+
+        Wilder Jean-claude = new Wilder("Jean-claude", false);
+          System.out.println("1. " + Jean-claude.whoAmI());
+
+        julien.setAware(true);
+          System.out.println("2. " + Henry.whoAmI());
+  
+  
+  }
+
+
+}
